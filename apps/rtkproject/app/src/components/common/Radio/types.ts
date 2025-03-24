72defeb2ba7a5fa6d116c5f7518a5f6c323fba3e
ekim <eunkukkim1992@gmail.com>
2025-03-25 00:21:20 +0900

    radio types update

--- apps/rtkproject/app/src/components/common/Radio/types.ts
+++ apps/rtkproject/app/src/components/common/Radio/types.ts
@@ -1,2 +1,12 @@
-{
+import { CSSObject } from 'styled-components';
+
+export interface RadioGroupProps {
+  name: string;
+  value: any;
+  onChange: (value: any) => void;
+  options: Array<{
+    value: any;
+    label: string;
+  }>;
+  $customStyles?: CSSObject;
 }
