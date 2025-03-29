092cfb0d9e706dd87a17242f3b4f915561c8517a
ekim <eunkukkim1992@gmail.com>
2025-03-29 21:57:17 +0900

    InfiniteScrollArea

---
+++ apps/rtkproject/app/src/components/InfiniteScroll/InfiniteScrollArea.tsx
@@ -0,0 +1,7 @@
+import InfiniteScroll from 'react-infinite-scroller';
+
+const InfiniteScrollArea = InfiniteScroll as any;
+
+InfiniteScrollArea.displayname = 'InfiniteScrollArea';
+
+export default InfiniteScrollArea;
