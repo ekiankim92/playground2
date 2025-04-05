13d0c86f5a35624860fce145032224a11b852415
ekim <eunkukkim1992@gmail.com>
2025-04-05 23:57:38 +0900

    arccodion styles

---
+++ apps/rtkproject/app/src/components/Arccodion/arccodion.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const AccordionItems = styled.div``;
+export const AccordionItem = styled.div``;
+export const AccordionTitle = styled.div``;
+export const AccordionExpandableContent = styled.div``;
+export const AccordionContent = styled.div``;
