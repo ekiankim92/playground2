ed24df9b3dbc0b99a3c9098602ded54f1614774a
ekim <eunkukkim1992@gmail.com>
2025-04-10 00:13:37 +0900

    Buttonts

---
+++ apps/rtkproject/app/src/components/Button/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Button 모듈 내 모든 항목을 재출력합니다.
+ *
+ * @description `Button` 모듈에서 내보낸 모든 함수, 클래스, 타입 등을 현재 파일에서 재출력합니다.
+ * - 이 코드를 통해 `Button` 모듈의 모든 내보낸 항목을 간단히 가져올 수 있습니다.
+ *
+ * @example
+ * ```
+ * import { Button, ButtonProps } from './Button';
+ * ```
+ */
+export * from './Button';
