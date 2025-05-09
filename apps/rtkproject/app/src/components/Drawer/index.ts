ad2758a1660c706fad409486d1bc5829baa1a90b
ekim <eunkukkim1992@gmail.com>
2025-05-09 20:08:02 +0900

    Drawer.ts

---
+++ apps/rtkproject/app/src/components/Drawer/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Drawer 컴포넌트와 관련된 타입들을 내보냅니다.
+ *
+ * Drawer - 화면의 측면에서 슬라이딩되어 나타나는 패널 컴포넌트
+ * DrawerProps - Drawer 컴포넌트의 속성 타입
+ * DrawerContainerProps - Drawer 컨테이너의 스타일 속성 타입
+ */
+export * from './Drawer';
