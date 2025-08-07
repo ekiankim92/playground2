8fd25ebbf555be14078bcb065e3cc18dcb520fa5
ekim <eunkukkim1992@gmail.com>
2025-08-08 00:00:37 +0900

    third tanstack table example

---
+++ apps/rtkproject/app/src/components/table/tanstack/table.3.tsx
@@ -0,0 +1,183 @@
+'use client';
+
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { useEffect, useState } from 'react';
+import './styles.css';
+
+type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  status: string;
+  progress: number;
+};
+
+const defaultData: Person[] = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+];
+
+const FIXED_COLUMN_SIZE = {
+  firstName: 80,
+  lastName: 130,
+};
+
+export default function SaaSPage() {
+  const columnHelper = createColumnHelper<Person>();
+  const [colWidth, setColWidth] = useState(0);
+
+  const columns = [
+    columnHelper.group({
+      id: 'hello',
+      header: () => <span>Hello</span>,
+      columns: [
+        columnHelper.accessor('firstName', {
+          cell: (info) => info.getValue(),
+          footer: (props) => props.column.id,
+          size: FIXED_COLUMN_SIZE.firstName,
+        }),
+        columnHelper.accessor((row) => row.lastName, {
+          id: 'lastName',
+          cell: (info) => info.getValue(),
+          header: () => <span>Last Name</span>,
+          footer: (props) => props.column.id,
+          size: FIXED_COLUMN_SIZE.lastName,
+        }),
+      ],
+    }),
+    columnHelper.group({
+      header: 'Info',
+      footer: (props) => props.column.id,
+      columns: [
+        columnHelper.accessor('age', {
+          header: () => 'Age',
+          footer: (props) => props.column.id,
+          size: colWidth,
+        }),
+        columnHelper.accessor('visits', {
+          header: () => <span>Visits</span>,
+          footer: (props) => props.column.id,
+          size: colWidth,
+        }),
+        columnHelper.accessor('status', {
+          header: 'Status',
+          footer: (props) => props.column.id,
+          size: colWidth,
+        }),
+        columnHelper.accessor('progress', {
+          header: 'Profile Progress',
+          footer: (props) => props.column.id,
+          size: colWidth,
+        }),
+      ],
+    }),
+  ];
+
+  const [data, _setData] = useState(() => [...defaultData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    enableColumnResizing: true,
+    columnResizeMode: 'onChange', // or 'onEnd' depending on preference
+  });
+
+  useEffect(() => {
+    const columnLength = table.getAllLeafColumns().length;
+    const fixedValue = Object.values(FIXED_COLUMN_SIZE);
+
+    const calcuateWidth = fixedValue.reduce((acc, cur) => {
+      return acc - cur;
+    }, window.innerWidth);
+
+    setColWidth(calcuateWidth / (columnLength - fixedValue.length));
+  }, [table]);
+
+  return (
+    <div className='p-2'>
+      {colWidth && (
+        <table style={{ tableLayout: 'fixed' }}>
+          <colgroup>
+            {table.getAllLeafColumns().map((col) => (
+              <col key={col.id} style={{ width: col.getSize() }} />
+            ))}
+          </colgroup>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      style={{
+                        position: 'relative',
+                      }}
+                    >
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+
+                      {header.column.getCanResize() && (
+                        <div
+                          onMouseDown={header.getResizeHandler()}
+                          onTouchStart={header.getResizeHandler()}
+                          className='resize-handle'
+                          style={{
+                            position: 'absolute',
+                            right: 0,
+                            top: 0,
+                            height: '100%',
+                            width: '5px',
+                            cursor: 'col-resize',
+                            zIndex: 1,
+                          }}
+                        />
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id} style={{ width: cell.column.getSize() }}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <div className='h-4' />
+    </div>
+  );
+}
--- apps/rtkproject/app/src/components/table/tanstack/table1.tsx
+++ apps/rtkproject/app/src/components/table/tanstack/table1.tsx
@@ -1,230 +1,90 @@
 'use client';
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
-import { useMemo, useState } from 'react';
-import './styles.css';
+import { getColWidth } from '@/components/organism/data-table/common/util/columnWidth';
+import PaginatedDataTable from '@/components/organism/data-table/PaginatedDataTable';
+import { useColumnFilters } from '@/hooks/useColumnFilters';
+import usePagination from '@/hooks/usePagination';
+import { useWindowSize } from '@/hooks/useWindowSize';
+import { useQuery } from '@tanstack/react-query';
+import { CellContext, RowData } from '@tanstack/react-table';
+import axios from 'axios';
+import '../../../../components/organism/data-table/common/styles/tableStyles.css';
 
-const myData = [
-  { firstName: 'John', lastName: 'Smith', age: 20, visits: 4, status: 'single', progress: 20 },
-  {
-    firstName: 'Emily',
-    lastName: 'Johnson',
-    age: 25,
-    visits: 10,
-    status: 'relationship',
-    progress: 55,
-  },
-  {
-    firstName: 'Michael',
-    lastName: 'Williams',
-    age: 32,
-    visits: 7,
-    status: 'complicated',
-    progress: 80,
-  },
-  { firstName: 'Sarah', lastName: 'Brown', age: 28, visits: 5, status: 'single', progress: 40 },
-  { firstName: 'David', lastName: 'Jones', age: 45, visits: 12, status: 'married', progress: 90 },
-  { firstName: 'Jessica', lastName: 'Garcia', age: 22, visits: 3, status: 'single', progress: 30 },
-  {
-    firstName: 'Daniel',
-    lastName: 'Miller',
-    age: 36,
-    visits: 8,
-    status: 'relationship',
-    progress: 70,
-  },
-  {
-    firstName: 'Laura',
-    lastName: 'Davis',
-    age: 27,
-    visits: 6,
-    status: 'complicated',
-    progress: 50,
-  },
-  { firstName: 'James', lastName: 'Martinez', age: 31, visits: 9, status: 'married', progress: 85 },
-  { firstName: 'Anna', lastName: 'Lopez', age: 24, visits: 2, status: 'single', progress: 25 },
-];
-
-type Person = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  visits: number;
-  status: string;
-  progress: number;
+const FIXED_COLUMN_SIZE = {
+  idx: 80,
+  occurredAt: 130,
 };
 
-const defaultColumns: ColumnDef<Person>[] = [
-  {
-    header: '이름',
-    columns: [
-      {
-        accessorKey: 'firstName',
-        header: '이름',
-        size: 200,
-        minSize: 50,
-        maxSize: 200,
-      },
-      {
-        accessorKey: 'lastName',
-        header: '성',
-        size: 150,
-      },
-    ],
-  },
-  {
-    header: '개인정보',
-    columns: [
-      {
-        accessorKey: 'age',
-        header: '나이',
-      },
-      {
-        accessorKey: 'visits',
-        header: '방문 횟수',
-      },
-    ],
-  },
-  {
-    header: '추가정보',
-    columns: [
-      {
-        accessorKey: 'status',
-        header: '상태',
-        size: 130,
-      },
-      {
-        accessorKey: 'progress',
-        header: '프로그래스',
-      },
-    ],
-  },
-];
+export default function SaaSPage() {
+  const { pagination, setPagination } = usePagination();
+  const { columnFilters, setColumnFilters } = useColumnFilters();
+  const { screenWidth } = useWindowSize();
 
-export default function DataFlowPage() {
-  const [data, _setData] = useState([...myData]);
-  const [columns] = useState<typeof defaultColumns>(() => [...defaultColumns]);
-  const [columnSizing, setColumnSizing] = useState({});
+  const colWidth = getColWidth({ fixedColumnSizes: FIXED_COLUMN_SIZE, columnLen: 5, screenWidth });
 
-  const table = useReactTable({
-    data,
-    columns,
-    state: {
-      columnSizing,
+  const { data } = useQuery({
+    queryKey: ['EnforceAppLockerAlarm', pagination],
+    queryFn: async () => {
+      const { offset, limit } = pagination;
+
+      const res = await axios.get(
+        `http://192.168.6.3/api/v1/enforce/applocker/alarm?hostname=&limit=${limit}&occurred_at=2025-04-30%2C2025-07-29&offset=${offset}&search=&value=`,
+      );
+
+      return res?.data;
     },
-    defaultColumn: {
-      minSize: 60,
-      maxSize: 800,
-    },
-    columnResizeMode: 'onChange',
-    getCoreRowModel: getCoreRowModel(),
-    debugTable: true,
-    debugHeaders: true,
-    debugColumns: true,
-
-    enableColumnResizing: true,
-
-    onColumnSizingChange: setColumnSizing,
   });
 
-  const columnSizeVars = useMemo(() => {
-    const headers = table.getFlatHeaders();
-    const colSizes: { [key: string]: number } = {};
-    for (let i = 0; i < headers.length; i++) {
-      const header = headers[i]!;
-      colSizes[`--header-${header.id}-size`] = header.getSize();
-      colSizes[`--col-${header.column.id}-size`] = header.column.getSize();
-    }
-    return colSizes;
-  }, [table]);
+  const columns = [
+    {
+      id: 'blank',
+      header: () => null,
+      columns: [
+        {
+          accessorKey: 'idx',
+          header: '아이디',
+          meta: {
+            filterVariant: 'text',
+          },
+          size: 80,
+        },
+        {
+          accessorKey: 'occurredAt',
+          header: '일시',
+          cell: ({ cell }: CellContext<RowData, string>) => {
+            const value = cell.getValue() as string;
+            return <div>{value.split('T')[0]}</div>;
+          },
+          size: 130,
+        },
+      ],
+    },
+    {
+      header: '계정',
+      columns: [
+        { accessorKey: 'ipAddress', header: 'IP Address', size: colWidth },
+        { accessorKey: 'hostname', header: 'Hostname', size: colWidth },
+      ],
+    },
+    {
+      header: '행위',
+      columns: [{ accessorKey: 'policyCode', header: '코드', size: colWidth }],
+    },
+  ];
 
   return (
-    <div className='overflow-x-auto'>
-      <table
-        {...{
-          style: {
-            width: table.getCenterTotalSize(),
-          },
-        }}
-      >
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  {...{
-                    // key: header.id,
-                    colSpan: header.colSpan,
-                    style: {
-                      width: header.getSize(),
-                    },
-                  }}
-                >
-                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  <div
-                    {...{
-                      onDoubleClick: () => header.column.resetSize(),
-                      onMouseDown: header.getResizeHandler(),
-                      className: `resizer ${table.options.columnResizeDirection} ${
-                        header.column.getIsResizing() ? 'isResizing' : ''
-                      }`,
-                    }}
-                  />
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td
-                  key={cell.id}
-                  {...{
-                    // key: cell.id,
-                    style: {
-                      width: cell.column.getSize(),
-                    },
-                  }}
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <div>SaaSPage</div>
+      <div>Table!</div>
+      <PaginatedDataTable
+        tableData={data?.data}
+        columns={columns}
+        columnFilters={columnFilters}
+        setColumnFilters={setColumnFilters}
+        pagination={pagination}
+        setPagination={setPagination}
+        totalCount={data?.count}
+        colWidth={colWidth}
+      />
     </div>
   );
 }
-
-//un-memoized normal table body component - see memoized version below
-// function TableBody({ table }: { table: Table<Person> }) {
-//   return (
-//     <div className="tbody">
-//       {table.getRowModel().rows.map((row) => (
-//         <div key={row.id} className="tr">
-//           {row.getVisibleCells().map((cell) => {
-//             return (
-//               <div
-//                 key={cell.id}
-//                 className="td"
-//                 // style={{ width: `calc(var(--col-${cell.column.id}-size) * 1px)` }}
-//                 style={{ width: `${cell.column.getSize()}px` }}
-//               >
-//                 {cell.renderValue<any>()}
-//               </div>
-//             );
-//           })}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-//special memoized wrapper for our table body that we will use during column resizing
-// export const MemoizedTableBody = memo(
-//   TableBody,
-//   (prev, next) => prev.table.options.data === next.table.options.data,
-// ) as typeof TableBody;
