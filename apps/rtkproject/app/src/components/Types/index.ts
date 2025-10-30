1da596231c5648e607bc093cc9cb9f886a44ca7b
ekim <eunkukkim1992@gmail.com>
2025-10-30 23:59:33 +0900

    types add

---
+++ apps/rtkproject/app/src/components/Types/index.ts
@@ -0,0 +1,11 @@
+export interface StyledUICommonProps {
+  $variant?: string;
+  $size?: string;
+  $colorPalette?: string;
+  $style?: React.CSSProperties;
+  children?: React.ReactNode;
+}
+
+export interface StyleProps {
+  children?: React.ReactNode;
+}
